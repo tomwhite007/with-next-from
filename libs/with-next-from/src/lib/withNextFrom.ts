825b77477386224b68fd7e96dd4ea6a47a1b1954
Tom White <tomwhite007@gmail.com>
2020-06-25 08:31:20 +0100

    using zip v1

--- libs/with-next-from/src/lib/withNextFrom.ts
+++ libs/with-next-from/src/lib/withNextFrom.ts
@@ -1,34 +1,21 @@
-import { Observable } from 'rxjs';
-import { take } from 'rxjs/operators';
+import { Observable, of, zip } from 'rxjs';
+import { take, switchMap, first } from 'rxjs/operators';
 
 export const withNextFrom = <N>(...args: Array<() => Observable<N>>) => <O>(
   source$: Observable<O>
 ) =>
-  new Observable<any[]>((observer) => {
-    return source$.subscribe({
-      next(o) {
-        let streamCount = 0;
-        const result: any[] = [o];
-        args.forEach((getNextStream: () => Observable<N>, argIndex: number) => {
-          getNextStream()
-            .pipe(take(1))
-            .subscribe({
-              next(n) {
-                streamCount++;
-                result[argIndex + 1] = n;
-                if (streamCount === args.length) observer.next(result);
-              },
-              error(err) {
-                observer.error(err);
-              },
-            });
-        });
-      },
-      error(err) {
-        observer.error(err);
-      },
-      complete() {
-        observer.complete();
-      },
-    });
+  new Observable((observer) => {
+    return source$
+      .pipe(switchMap((o) => zip(of(o), ...args.map((f) => f().pipe(first())))))
+      .subscribe({
+        next(o) {
+          observer.next(o);
+        },
+        error(err) {
+          observer.error(err);
+        },
+        complete() {
+          observer.complete();
+        },
+      });
   });
