b2e26a69dba979bb89d6630f3b229a81ade60df9
Tom White <tomwhite007@gmail.com>
2020-06-27 12:14:38 +0100

    refactored to continue after source completes

--- libs/with-next-from/src/lib/withNextFrom.spec.ts
+++ libs/with-next-from/src/lib/withNextFrom.spec.ts
@@ -1,5 +1,6 @@
 import { hot, cold } from '@nrwl/angular/testing';
 import { withNextFrom } from './withNextFrom';
+import { withLatestFrom } from 'rxjs/operators';
 
 describe('withNextFrom', () => {
   // prettier-ignore
@@ -17,6 +18,69 @@ describe('withNextFrom', () => {
         () => e2$,
         () => e3$,
         () => e4$
+      )
+    );
+
+    expect(result).toBeObservable(expected);
+  });
+
+  // prettier-ignore
+  it('should continue to wait for parameter streams to complete if originator stream completes', () => {
+    const e1$ =      cold('-----a|');
+    const e2$ =       hot('--b-----c---d------|');
+    const e3$ =       hot('e------f---g---h------|');
+    const e4$ =      cold('--(i|)');
+    const expected = cold('--------(x|)', {
+      x: ['a', 'c', 'f', 'i'],
+    });
+
+    const result = e1$.pipe(
+      withNextFrom(
+        () => e2$,
+        () => e3$,
+        () => e4$
+      )
+    );
+
+    expect(result).toBeObservable(expected);
+  });
+
+  // prettier-ignore
+  it('should complete with no result', () => {
+    const e1$ =      cold('-----|');
+    const e2$ =       hot('--b-----c---d------|');
+    const e3$ =       hot('e------f---g---h------|');
+    const e4$ =      cold('--(i|)');
+    const expected = cold('-----|', {
+      x: ['a', 'c', 'f', 'i'],
+    });
+
+    const result = e1$.pipe(
+      withNextFrom(
+        () => e2$,
+        () => e3$,
+        () => e4$
+      )
+    );
+
+    expect(result).toBeObservable(expected);
+  });
+
+  // prettier-ignore
+  xit('should fail because withLatestFrom completes when the source does', () => {
+    const e1$ =      cold('-----a|');
+    const e2$ =       hot('--b-------------d------|');
+    const e3$ =       hot('e--------------g---h------|');
+    const e4$ =      cold('-------i|');
+    const expected = cold('-------x|', {
+      x: ['a', 'b', 'e', 'i'],
+    });
+
+    const result = e1$.pipe(
+      withLatestFrom(
+        e2$,
+        e3$,
+        e4$
       )
     );
 
--- libs/with-next-from/src/lib/withNextFrom.ts
+++ libs/with-next-from/src/lib/withNextFrom.ts
@@ -1,21 +1,57 @@
-import { Observable, of, zip } from 'rxjs';
-import { take, switchMap, first } from 'rxjs/operators';
+import {
+  Observable,
+  of,
+  zip,
+  Subject,
+  onErrorResumeNext,
+  throwError,
+} from 'rxjs';
+import { take, switchMap, first, map } from 'rxjs/operators';
 
 export const withNextFrom = <N>(...args: Array<() => Observable<N>>) => <O>(
   source$: Observable<O>
 ) =>
   new Observable((observer) => {
-    return source$
-      .pipe(switchMap((o) => zip(of(o), ...args.map((f) => f().pipe(first())))))
-      .subscribe({
-        next(o) {
-          observer.next(o);
-        },
-        error(err) {
-          observer.error(err);
-        },
-        complete() {
-          observer.complete();
-        },
-      });
+    let currentSourceValue: any;
+
+    // return source$
+    //   .pipe(
+    //     switchMap((o) => {
+    //       currentSourceValue = o;
+    //       return zip(of(o), ...args.map((f) => f().pipe(first())));
+    //     })
+    //   )
+    //   .subscribe({
+    //     next(res) {
+    //       observer.next(res);
+    //     },
+    //     error(err) {
+    //       observer.error(err);
+    //     },
+    //     complete() {
+    //       observer.complete();
+    //     },
+    //   });
+
+    return onErrorResumeNext(
+      source$.pipe(
+        switchMap((o) => {
+          currentSourceValue = o;
+          return zip(of(o), ...args.map((f) => f().pipe(first())));
+        })
+      ),
+      currentSourceValue !== undefined
+        ? zip(of(currentSourceValue), ...args.map((f) => f().pipe(first())))
+        : of()
+    ).subscribe({
+      next(res) {
+        observer.next(res);
+      },
+      error(err) {
+        observer.error(err);
+      },
+      complete() {
+        observer.complete();
+      },
+    });
   });
